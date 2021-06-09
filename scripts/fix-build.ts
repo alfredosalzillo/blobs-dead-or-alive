7f9c153de52c18922c1093557626f1594af532ea
alfredo.salzillo <noctisbels@gmai.com>
2021-06-09 19:43:53 +0200

    added pwa service worker

--- scripts/fix-build.ts
+++ scripts/fix-build.ts
@@ -1,3 +1,4 @@
-const file = './dist/index.html';
-const data = Deno.readTextFileSync(file);
-Deno.writeTextFileSync(file, data.replace(/"\/deps/ig, '"./deps'))
+const indexPath = './dist/index.html';
+const serviceWorkerPath = './dist/service-worker.js';
+Deno.writeTextFileSync(indexPath, Deno.readTextFileSync(indexPath).replace(/"\/deps/ig, '"./deps'))
+Deno.writeTextFileSync(serviceWorkerPath, Deno.readTextFileSync(serviceWorkerPath).replace(/export default/ig, ''))
