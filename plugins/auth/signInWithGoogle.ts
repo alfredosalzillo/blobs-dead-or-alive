c097b3f9f936c81263bb3c04cf2242021b1b3bae
alfredosalzillo <noctisbels@gmail.com>
2023-04-30 09:40:07 +0200

    fix: use sign in with redirect

--- plugins/auth/signInWithGoogle.ts
+++ plugins/auth/signInWithGoogle.ts
@@ -1,11 +1,11 @@
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithRedirect } from 'firebase/auth';
 
 import app from '@/plugins/firebase';
 
 const auth = getAuth(app);
 const signInWithGoogle = () => {
   const provider = new GoogleAuthProvider();
-  return signInWithPopup(auth, provider);
+  return signInWithRedirect(auth, provider);
 };
 
 export default signInWithGoogle;
