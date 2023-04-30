67885bdb10efa8b5a4ae167a9d0376e678373211
alfredosalzillo <noctisbels@gmail.com>
2023-04-30 12:04:30 +0200

    feat: added twa generation folder

---
+++ twa/app/src/main/java/me/alfredosalzillo/blobsdeadoralive/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package me.alfredosalzillo.blobsdeadoralive.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
