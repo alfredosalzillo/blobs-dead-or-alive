62e153ae2cd2f8ed35aedbd7372b3c352f1734a0
alfredosalzillo <noctisbels@gmail.com>
2023-05-07 10:48:22 +0200

    fix(header): use router back on back button

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import React from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 import SmallLogo from '@/components/SmallLogo';
 import Typography from '@/components/Typography';
@@ -10,14 +13,28 @@ export type HeaderProps = {
   title: React.ReactNode;
   back?: boolean;
 }
-const Header: React.FC<HeaderProps> = ({ title, back }) => (
-  <header className={classes.root}>
-    {!back && <SmallLogo />}
-    {back && <Link href="/" className={classes.back}>{'<'}</Link>}
-    <Typography variant="body" component="h1">
-      {title}
-    </Typography>
-  </header>
-);
+const Header: React.FC<HeaderProps> = ({ title, back }) => {
+  const router = useRouter();
+  return (
+    <header className={classes.root}>
+      {!back && <SmallLogo />}
+      {back && (
+      <Link
+        href="/"
+        onClick={(e) => {
+          e.preventDefault();
+          router.back();
+        }}
+        className={classes.back}
+      >
+        {'<'}
+      </Link>
+      )}
+      <Typography variant="body" component="h1">
+        {title}
+      </Typography>
+    </header>
+  );
+};
 
 export default Header;
